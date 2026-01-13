277504ba0405e373858ec5c66201c8d8ab56dab6
Aditya Revanda. R <adirevanda4@gmail.com>
2026-01-13 18:43:34 +0700

    Add files via upload

---
+++ Python RNG mini game.py
@@ -0,0 +1,35 @@
+import random
+
+welcome_message = ' Welcome bro '
+lokasi_air = random.randint(1,4)
+
+print ('*****************')
+print (f'**{welcome_message}**')
+print ('*****************')
+
+nama = input('masukkan nama anda : ')
+
+print (f'''
+       
+hallo {nama} selamat datang di mini game!!! 
+lihat ke empat gelas berikut!!! 
+
+       |   | |   | |   | |   |
+       |_1_| |_2_| |_3_| |_4_|
+       
+       ''')
+
+while True:
+    pilihan_user = int(input('''silahkan pilih di gelas nomer berapa yang berisi air? 1/2/3/4 : '''))
+    
+    if pilihan_user == lokasi_air :
+        print (f'SELAMAT KAMU MENANG!!! gelas yang berisi air berada di no : {lokasi_air} sedangkan pilihanmu adalah gelas no : {pilihan_user}')
+        break
+   
+    elif pilihan_user > 4 :
+        print (f'KOCAK!!! Cuman sampe 4 BOSSS!!! Ulangi lagi')
+        continue
+    
+    else :
+        print (f'Kamu kalah!!! gelas yang berisi air beradaada di no : {lokasi_air} sedangkan pilihanmu adalah gelas no : {pilihan_user}')
+        break
