2f743c4b7bad34c57c2afed8e39bd0a24599e7bb
Aditya Revanda. R <adirevanda4@gmail.com>
2026-01-13 19:28:34 +0700

    Python RNG mini game.py

--- Python RNG mini game.py
+++ Python RNG mini game.py
@@ -21,15 +21,26 @@ lihat ke empat gelas berikut!!!
 
 while True:
     pilihan_user = int(input('''silahkan pilih di gelas nomer berapa yang berisi air? 1/2/3/4 : '''))
+    print()
+    konfirmasi = input('Apakah kamu yakin dengan pilihanmu? Yes/No : ')
     
-    if pilihan_user == lokasi_air :
-        print (f'SELAMAT KAMU MENANG!!! gelas yang berisi air berada di no : {lokasi_air} sedangkan pilihanmu adalah gelas no : {pilihan_user}')
+    if konfirmasi == 'Yes':
         break
-   
-    elif pilihan_user > 4 :
-        print (f'KOCAK!!! Cuman sampe 4 BOSSS!!! Ulangi lagi')
+    elif konfirmasi == 'No':
         continue
+    else:
+        print ('''
+Pilih jawaban antara Yes/No dan lihat huruf besar dan kecilnya!!!
+''')
+        
+print()
+if pilihan_user == lokasi_air :
+    print (f'SELAMAT KAMU MENANG!!! gelas yang berisi air berada di no : {lokasi_air} sedangkan pilihanmu adalah gelas no : {pilihan_user}')
+        
+elif pilihan_user > 4 :
+    print (f'KOCAK!!! Cuman sampe 4 BOSSS!!! Jawaban yang benar ialah di gelas no : {lokasi_air}')
+        
     
-    else :
-        print (f'Kamu kalah!!! gelas yang berisi air beradaada di no : {lokasi_air} sedangkan pilihanmu adalah gelas no : {pilihan_user}')
-        break
+else :
+    print (f'Kamu kalah!!! gelas yang berisi air beradaada di no : {lokasi_air} sedangkan pilihanmu adalah gelas no : {pilihan_user}')
+        
